2672be210713aa184be090f695564d5f71a74304
Heiko Strathmann <heiko.strathmann@gmail.com>
2013-09-19 20:38:10 +0100

    change to shogun log-det

--- ozone/scripts/sample_ozone_posterior_average_serial.py
+++ ozone/scripts/sample_ozone_posterior_average_serial.py
@@ -36,6 +36,7 @@ def main():
     posterior = OzonePosteriorAverageEngine(computation_engine=computation_engine,
                                         num_estimates=num_estimates,
                                         prior=prior)
+    posterior.logdet_method="shogun_estimate"
     
     proposal_cov = diag([ 4.000000000000000e-05, 1.072091680000000e+02])
     mcmc_sampler = StandardMetropolis(posterior, scale=1.0, cov=proposal_cov)
