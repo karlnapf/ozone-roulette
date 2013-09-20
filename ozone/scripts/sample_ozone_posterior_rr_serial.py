d4c252735d44998fd2d12c2fa625cee90c94db1c
Heiko Strathmann <heiko.strathmann@gmail.com>
2013-09-20 16:27:48 +0100

    test case with simulated noise to be ran locally

--- ozone/scripts/sample_ozone_posterior_rr_serial.py
+++ ozone/scripts/sample_ozone_posterior_rr_serial.py
@@ -7,41 +7,45 @@ the Free Software Foundation; either version 3 of the License, or
 Written (W) 2013 Heiko Strathmann
 """
 
+from engines.SGEComputationEngine import SGEComputationEngine
 from engines.SerialComputationEngine import SerialComputationEngine
-from os.path import expanduser
 from main.distribution.Gaussian import Gaussian
 from main.mcmc.MCMCChain import MCMCChain
 from main.mcmc.MCMCParams import MCMCParams
-from main.mcmc.output.PlottingOutput import PlottingOutput
 from main.mcmc.output.StatisticsOutput import StatisticsOutput
 from main.mcmc.output.StoreChainOutput import StoreChainOutput
 from main.mcmc.samplers.StandardMetropolis import StandardMetropolis
 from numpy.lib.twodim_base import diag, eye
 from numpy.ma.core import asarray
+from os.path import expanduser
+from ozone.distribution.OzonePosteriorAverageEngine import \
+    OzonePosteriorAverageEngine
 from ozone.distribution.OzonePosteriorRREngine import OzonePosteriorRREngine
 from pickle import dump
-from russian_roulette.RussianRouletteSubSampling import \
-    RussianRouletteSubSampling
+from russian_roulette.RussianRoulette import RussianRoulette
 from tools.Log import Log
 import logging
 import os
 
-
 def main():
     Log.set_loglevel(logging.DEBUG)
+    
+    prior = Gaussian(Sigma=eye(2) * 100)
+    num_estimates = 5
     
     home = expanduser("~")
     folder = os.sep.join([home, "sample_ozone_posterior_rr_serial"])
     
-    prior = Gaussian(Sigma=eye(2) * 100)
-    num_estimates = 2
-    
     computation_engine = SerialComputationEngine()
-    rr_instance = RussianRouletteSubSampling(1e-5, block_size=1, num_desired_estimates=num_estimates)
+    
+    rr_instance = RussianRoulette(1e-5, block_size=1)
+    
     posterior = OzonePosteriorRREngine(rr_instance=rr_instance,
                                        computation_engine=computation_engine,
                                        num_estimates=num_estimates,
                                        prior=prior)
+    
+    posterior.logdet_method = "shogun_exact_plus_noise"
     
     proposal_cov = diag([ 4.000000000000000e-05, 1.072091680000000e+02])
     mcmc_sampler = StandardMetropolis(posterior, scale=1.0, cov=proposal_cov)
@@ -50,10 +54,10 @@ def main():
     mcmc_params = MCMCParams(start=start, num_iterations=2000)
     chain = MCMCChain(mcmc_sampler, mcmc_params)
     
-    chain.append_mcmc_output(PlottingOutput(None, plot_from=1, lag=1))
+#    chain.append_mcmc_output(PlottingOutput(None, plot_from=1, lag=1))
     chain.append_mcmc_output(StatisticsOutput(print_from=1, lag=1))
     
-    store_chain_output = StoreChainOutput(folder)
+    store_chain_output = StoreChainOutput(folder, lag=50)
     chain.append_mcmc_output(store_chain_output)
     
     loaded = store_chain_output.load_last_stored_chain()
