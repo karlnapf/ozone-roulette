bdb4c54ee94271d62363ebfef21d3617599ea7eb
Heiko Strathmann <heiko.strathmann@gmail.com>
2014-04-19 20:30:09 +0100

    added error message for overflows

--- ozone/distribution/OzonePosteriorAverageEngine.py
+++ ozone/distribution/OzonePosteriorAverageEngine.py
@@ -26,14 +26,17 @@ The views and conclusions contained in the software and documentation are those
 of the authors and should not be interpreted as representing official policies,
 either expressed or implied, of the author.
 """
-from aggregators.ScalarResultAggregator import ScalarResultAggregator
+import logging
+from numpy import where
 from numpy.ma.core import log, zeros
+
+from aggregators.ScalarResultAggregator import ScalarResultAggregator
 from ozone.distribution.OzonePosterior import OzonePosterior
 from ozone.distribution.OzonePosteriorAverage import OzonePosteriorAverage
 from ozone.jobs.OzoneLikelihoodWithoutLogDetJob import \
     OzoneLikelihoodWithoutLogDetJob
 from ozone.jobs.OzoneLogDetJob import OzoneLogDetJob
-import logging
+
 
 class OzonePosteriorAverageEngine(OzonePosteriorAverage):
     def __init__(self, computation_engine, num_estimates, prior):
@@ -69,8 +72,8 @@ class OzonePosteriorAverageEngine(OzonePosteriorAverage):
         for i in range(self.num_estimates):
             aggregators_Q[i].finalize()
             aggregators_M[i].finalize()
-            log_dets_Q[i]=aggregators_Q[i].get_final_result().result
-            log_dets_M[i]=aggregators_M[i].get_final_result().result
+            log_dets_Q[i] = aggregators_Q[i].get_final_result().result
+            log_dets_M[i] = aggregators_M[i].get_final_result().result
             aggregators_Q[i].clean_up()
             aggregators_M[i].clean_up()
             
@@ -85,6 +88,26 @@ class OzonePosteriorAverageEngine(OzonePosteriorAverage):
         # construct all likelihood estimates
         log_det_parts = 0.5 * log_dets_Q + 0.5 * n * log(tau) - 0.5 * log_dets_M
         estimates = log_det_parts + result_remainder
+        
+        # crude check for an overflow to print error details
+        limit = 1e100
+        indices = where(abs(estimates) > limit)[0]
+        if len(indices) > 0:
+            logging.info("Log-likelihood estimates overflow occured at the following indices:")
+            for idx in indices:
+                logging.info("At index %d. Details are: " % idx)
+                logging.info("log-det Q: " + aggregators_Q[idx].job_name + 
+                             ". Result is %f" % log_dets_Q[idx])
+                logging.info("log-det M: " + aggregators_M[idx].job_name + 
+                             ". Result is %f" % log_dets_M[idx])
+                logging.info("log-lik-without-log-det: " + 
+                             aggregator_remainder[idx].job_name + ". Result is %f" % result_remainder[idx])
+                
+            logging.info("Removing mentioned estimates from list")
+            estimates = estimates[abs(estimates) < limit]
+            logging.info("New number of estimates is %d, old was %d" % 
+                         (len(estimates), self.num_estimates))
+                
         
         logging.debug("Leaving")
         return estimates
